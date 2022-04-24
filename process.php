a115cb48e948134edf94e0643764613b3aa1391e
Showmick Kar <showmick12@gmail.com>
2022-04-25 02:48:45 +0600

    Update process.php

--- process.php
+++ process.php
@@ -5,7 +5,7 @@ $mysqli = new mysqli('localhost', 'root', '', 'bracu_mates') or die(mysqli_error
 
 /* TODO: STUDENT REGISTRATION AND LOGIN */
 
-if (isset($_POST['register_donor'])) {
+if (isset($_POST['register_student'])) {
     $name = $_POST['name'];
     $school = $_POST['school'];
     $college = $_POST['college'];
@@ -14,9 +14,11 @@ if (isset($_POST['register_donor'])) {
     $linkedin = $_POST['linkedin'];
     $department = $_POST['department'];
     $dob = $_POST['dob'];
+    $email = $_POST['email'];
     $current_location = $_POST['current_location'];
     $credits_completed = $_POST['credits_completed'];
     $password = $_POST['password'];
+
     $clubs = $_POST['club[]'];
     $fields_of_interest = $_POST['field_of_interest'];
     $hobbies = $_POST['hobby'];
@@ -32,21 +34,47 @@ if (isset($_POST['register_donor'])) {
             $mysqli->query("INSERT INTO student(name, school, college, hometown, blood_group, linkedin, department, dob, current_location, credits_completed, password) VALUES('$name','$school','$college', '$hometown', '$blood_group', '$linkedin', '$department', '$dob', '$current_location', '$credits_completed', '$password')") or die($mysqli->error);
 
             // Store the area of expertise in alumni_area_of_expertise table
-            $sql = "SELECT id FROM `donor` WHERE `email` LIKE '$email'";
+            $sql = "SELECT id FROM `student` WHERE `email` LIKE '$email'";
             $result = $mysqli->query($sql);
             $row = mysqli_fetch_array($result);
-            $donor_id = $row[0];
+            $student_id = $row[0];
 
-            foreach ($available_locations as $location) {
-                $mysqli->query("INSERT INTO `donor_available_location` (`d_id`, `location`) VALUES ('$donor_id','$location');");
+            foreach ($clubs as $club) {
+                $mysqli->query("INSERT INTO `student_club` (`s_id`, `club`) VALUES ('$student_id','$club');");
             }
 
-            foreach ($health_conditions as $health_condition) {
-                $mysqli->query("INSERT INTO `donor_health_condition` (`d_id`, `health_condition`) VALUES ('$donor_id','$health_condition');");
+            foreach ($fields_of_interest as $field_of_interest) {
+                $mysqli->query("INSERT INTO `student_field_of_interest` (`s_id`, `field_of_interest`) VALUES ('$student_id','$field_of_interest');");
+            }
+
+            foreach ($hobbies as $hobby) {
+                $mysqli->query("INSERT INTO `student_hobby` (`s_id`, `hobby`) VALUES ('$student_id','$hobby');");
             }
         }
     }
 }
+
+// Student Login
+
+if (isset($_POST['login_student'])) {
+    $email = $_POST["email"];
+    $password = $_POST["password"];
+
+    $sql = "SELECT * FROM student WHERE  email='$email' AND password='$password'";
+    $result = $mysqli->query($sql);
+
+    $row = $result->fetch_assoc();
+    if ($result) {
+
+        if (mysqli_num_rows($result) >= 1) {
+            header("Location: student_profile.php");
+            exit();
+        } else {
+            echo "FAIL";
+        }
+    }
+}
+
 
 
 // Donor Registration
