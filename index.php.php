05f2d251cce97bbfd3ba3e500f26ee62a922b987
Showmick Kar <showmick12@gmail.com>
2022-04-26 00:48:27 +0600

    Update and rename homepage.php to index.php.php

--- index.php.php
+++ index.php.php
@@ -14,7 +14,6 @@
         <ul>
             <li><a href="registration.php">Register</a></li>
             <li><a href="login.php">Login</a></li>
-            <li><a href="search.php">Search</a></li>
         </ul>
     </nav>
 
