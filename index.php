1d52fb630a68ac6cd9ce55b28e0faa6a6a7477e7
eftykhairul <68026127+eftykhairul@users.noreply.github.com>
2022-04-26 07:22:41 +0600

    Update index.php

--- index.php
+++ index.php
@@ -6,17 +6,24 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
 
 <body>
-    <h1>WELCOME TO BRACU MATES</h1>
-    <nav>
+    <h1 class='text-center mt-4'>WELCOME TO BRACU MATES</h1>
+    <!-- <nav>
         <ul>
             <li><a href="registration.php">Register</a></li>
             <li><a href="login.php">Login</a></li>
+            <li><a href="search.php">Search</a></li>
         </ul>
-    </nav>
-
+    </nav> -->
+    <div class='text-center p-4'>
+        <a href="registration.php" class="btn btn-primary mb-4">Register</a> </br>
+        <a href="login.php" class="btn btn-success mb-4">Login</a></br>
+        <!-- <a href="search.php" class="btn btn-primary ">Search</a> -->
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </body>
 
 </html>
