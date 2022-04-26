f7e9f15531f9ecca4add58b2fc4f338c88f653f3
eftykhairul <68026127+eftykhairul@users.noreply.github.com>
2022-04-26 07:31:10 +0600

    Add files via upload

---
+++ student_profile.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="styles.css">
+    <title>Donor Profile</title>
+</head>
+
+<body>
+    <h1 class='text-center mt-4'>THIS IS DONOR PROFILE PAGE</h1>
+    <!-- <nav>
+        <ul>
+            <li><a href="index.php">logout</a></li>
+            <li><a href="faq.php">Frequently Asked Questions</a></li>
+        </ul>
+    </nav> -->
+    <div class='text-center p-4'>
+        <a href="search.php" class="btn btn-primary mb-4">Search</a></br>
+        <a href="faq.php" class="btn btn-success mb-4">Frequently Asked Questions</a></br>
+        <a href="index.php" class="btn btn-primary mb-4">Logout</a> 
+        
+        
+    </div>
+</body>
+
+</html>
