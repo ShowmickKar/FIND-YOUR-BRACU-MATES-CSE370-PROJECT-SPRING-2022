b1569132550a4b7fec196b1c2dcb2babec12f27a
Showmick Kar <showmick12@gmail.com>
2022-04-26 10:02:09 +0600

    Update donor_login.php

--- donor_login.php
+++ donor_login.php
@@ -22,7 +22,7 @@
             </div>
             <div class="form-group">
                 <label for="password">
-                    Donor Email
+                    Donor Password
                 </label>
                 <input type="password" name="password" placeholder="Your password" value="">
             </div>
