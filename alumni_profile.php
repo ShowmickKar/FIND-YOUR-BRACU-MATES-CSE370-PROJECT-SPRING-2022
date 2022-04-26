70124cd8990f87534cf92f2a720c277463a3156a
Showmick Kar <showmick12@gmail.com>
2022-04-26 07:58:13 +0600

    Update alumni_profile.php

--- alumni_profile.php
+++ alumni_profile.php
@@ -7,12 +7,18 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <link rel="stylesheet" href="styles.css">
-    <title>Document</title>
+    <title>Alumni Profile</title>
 </head>
 
-<body>
+<body>s
     <h1>THIS IS ALUMNI PROFILE PAGE</h1>
-    <nav><a href="homepage.php">logout</a></nav>
+    <nav>
+        <ul>
+            <li><a href="index.php">logout</a></li>
+            <li><a href="faq.php">Frequently Asked Questions</a></li>
+            <li><a href="lost_and_found.php">Lost & Found</a></li>
+        </ul>
+    </nav>
 </body>
 
 </html>
