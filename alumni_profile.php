af5ae76e336e1610d660fedc55c75d8659821900
Showmick Kar <showmick12@gmail.com>
2022-04-26 10:16:28 +0600

    Update alumni_profile.php

--- alumni_profile.php
+++ alumni_profile.php
@@ -7,18 +7,25 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <link rel="stylesheet" href="styles.css">
-    <title>Alumni Profile</title>
+    <title>Donor Profile</title>
 </head>
 
-<body>s
-    <h1>THIS IS ALUMNI PROFILE PAGE</h1>
-    <nav>
+<body>
+    <h1 class='text-center mt-4'>THIS IS ALUMNI PROFILE PAGE</h1>
+    <!-- <nav>
         <ul>
             <li><a href="index.php">logout</a></li>
             <li><a href="faq.php">Frequently Asked Questions</a></li>
-            <li><a href="lost_and_found.php">Lost & Found</a></li>
         </ul>
-    </nav>
+    </nav> -->
+    <div class='text-center p-4'>
+        <a href="search.php" class="btn btn-primary mb-4">Search</a></br>
+        <a href="faq.php" class="btn btn-success mb-4">Frequently Asked Questions</a></br>
+        <a href="index.php" class="btn btn-primary mb-4">Logout</a>
+         
+        
+    
+    </div>
 </body>
 
 </html>
